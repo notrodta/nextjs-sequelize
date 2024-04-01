6432b4fa395a3e40f1898a9a0f5cd3050ceaa344
Rod <rody101@gmail.com>
2024-04-01 14:54:39 -0400

    updated team api to create users as well

--- src/app/api/team/route.ts
+++ src/app/api/team/route.ts
@@ -1,19 +1,26 @@
 import sequelize from "@/db_connection";
 import Team from "@/models/Team";
+import User from "@/models/User";
 import { type NextRequest } from "next/server";
 
-export async function GET(request:NextRequest) {
-    await sequelize.sync({ alter: true });
-    const teams = await Team.findAll();
-    console.log(teams.every((user: any) => user instanceof Team));
-    console.log("all Users:", JSON.stringify(teams, null, 2));
-    return Response.json(teams);
+export async function GET(request: NextRequest) {
+  await sequelize.sync();
+  const teams = await Team.findAll({include: ['users']});
+//   console.log(teams.every((user: any) => user instanceof Team));
+//   console.log("all Users:", JSON.stringify(teams, null, 2));
+//   console.log("team user!!!!");
+//   console.log(teams[0]);
+
+  return Response.json(teams);
 }
 
 export async function POST(request: NextRequest) {
-    await sequelize.sync({ alter: true });
-    const team = await Team.create({ name: 'Team 3' });
-    console.log(team instanceof Team)
-    console.log(team.name);
-    return Response.json(team);
+  await sequelize.sync();
+  const team = await Team.create(
+    { name: "Team 3", users: [{ name: "bob" }, {name: 'sam'}] },
+    { include: [User] }
+  );
+  console.log(team instanceof Team);
+  console.log(team.name);
+  return Response.json(team);
 }
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,7 +3,7 @@ import User from "@/models/User";
 import { type NextRequest } from "next/server";
 
 export async function GET(request:NextRequest) {
-    await sequelize.sync({ alter: true });
+    await sequelize.sync();
     const users = await User.findAll();
     console.log(users.every((user: any) => user instanceof User));
     console.log("all Users:", JSON.stringify(users, null, 2));
@@ -11,7 +11,7 @@ export async function GET(request:NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
-    await sequelize.sync({ alter: true });
+    await sequelize.sync();
     const user = await User.create({ name: 'Jane' });
     console.log(user instanceof User)
     console.log(user.name);
